9aacd612d849c3118ca38ddc361db6a91c431b2d
Rohit <rohitnarra2@gmail.com>
2021-09-05 12:54:00 +0530

    adding filter ans select function.

--- college_analysis.py
+++ college_analysis.py
@@ -1,13 +1,9 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import col
 sc = SparkSession.builder.appName('sparkpractice').getOrCreate()
 df = sc.read.format("csv").option('header','true').option('inferschema','true').load("files/ForbesAmericasTopColleges2019.csv")
 df.printSchema()
-df.show(20)
-df.printSchema()
-df.describe()
-
-
-
-
+df.show(50)
+df.filter(col("state") == 'DC')
+df.select('Rank','Name','City','State').show()
 
