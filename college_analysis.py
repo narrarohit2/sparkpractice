89a5e8f6f58463691896bc32754ae0361e10f431
Rohit <rohitnarra2@gmail.com>
2021-09-05 13:09:09 +0530

    adding groupby function.

--- college_analysis.py
+++ college_analysis.py
@@ -1,9 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-sc = SparkSession.builder.appName('sparkpractice').getOrCreate()
+sc = SparkSession.builder.master("local").appName('sparkpractice').getOrCreate()
 df = sc.read.format("csv").option('header','true').option('inferschema','true').load("files/ForbesAmericasTopColleges2019.csv")
 df.printSchema()
 df.show(50)
-df.filter(col("state") == 'DC')
-df.select('Rank','Name','City','State').show()
-
+df2 = df
+df2.filter(col("state") == 'DC')
+df2.select('Rank','Name','City','State').show()
+df.show()
+df.groupBy('state').count().show()
